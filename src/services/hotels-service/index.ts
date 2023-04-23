a0a4953aa2102d1a6f82bb565d3d464abbc405dd
gavuolo <gabs.vuolo@gmail.com>
2023-04-22 22:47:31 -0400

    Fix: add await in hotels-service AllHotels funcion
    Signed-off-by: gavuolo <gabs.vuolo@gmail.com>

--- src/services/hotels-service/index.ts
+++ src/services/hotels-service/index.ts
@@ -5,7 +5,7 @@ import ticketsRepository from '@/repositories/tickets-repository';
 import { Ticket, TicketType, Hotel } from '@prisma/client';
 
 async function allHotels(userId: number){
-    const ticket = checkEnrollmentAndTicket(userId)
+    const ticket = await checkEnrollmentAndTicket(userId)
 
     //verificar o ticketType
     await checkTicketType(ticket)
@@ -15,24 +15,23 @@ async function allHotels(userId: number){
     const hotels = await hotelsRepository.findAllHotels()
     return hotels
 }
-async function checkPaymentStatus(ticket: Ticket & {TicketType: TicketType}){
+async function checkPaymentStatus(ticket: Ticket & {TicketType:TicketType}){
     if (ticket.status !== 'PAID') {
         throw new Error('The ticket has not yet been paid') 
     }
 }
-async function checkTicketType(ticket: Ticket & {TicketType: TicketType}) {
-
+async function checkTicketType(ticket: Ticket & {TicketType:TicketType}) {
     if (!ticket.TicketType.includesHotel) {
         throw new Error('Ticket type does not include hotel') 
     }
 }
-async function checkEnrollmentAndTicket(userId: number){
+async function checkEnrollmentAndTicket(userId: number): Promise<Ticket & {TicketType: TicketType}>{
     const enrollment = await enrollmentRepository.findWithAddressByUserId(userId)
     if(!enrollment) {
         throw notFoundError
     }
     const enrollmentId = enrollment.id
-    const ticket = await ticketsRepository.findTicketByEnrollmentId(enrollmentId)
+    const ticket: Ticket & {TicketType: TicketType} = await ticketsRepository.findTicketByEnrollmentId(enrollmentId)
     if(!ticket) {
         throw notFoundError
     }
